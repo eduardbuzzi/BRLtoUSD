bbe3ff89258fcf66f0f52b4862acd71657edb2e2
Eduardo <53496439+eduardbuzzi@users.noreply.github.com>
2020-03-18 16:17:03 -0300

    Update execute.sh

--- execute.sh
+++ execute.sh
@@ -9,7 +9,6 @@ curl -s -C - -o dolarhoje https://www.dolarhoje.com.br/dolar-turismo
 dolarTURISMO=$(grep 'class="peq"' dolarhoje | cut -d '>' -f4 | cut -d ' ' -f3 | tr , .)
 dolarTURISMOdata=$(grep "Atualizado" dolarhoje | cut -d '>' -f2 | cut -d '<' -f1 | cut -d ' ' -f1,2,3,5)
 rm dolarhoje
-echo
 echo "==={ DOLAR COMERCIAL }==="
 echo '$1.00 equivale a R$'"$dolarCOMERCIAL"
 echo "$dolarCOMERCIALdata"
@@ -33,7 +32,7 @@ echo "Caso não deseje, apenas deixe em branco e pressione Enter."
 echo "Seu caminho atual de padrao esta em $PWD"
 read -p "Local onde quer salvar/nome do arquivo: " LOCAL
 test "$LOCAL" = "" && exit
-echo > $LOCAL
+echo >> $LOCAL
 echo "==={ DOLAR COMERCIAL }===" >> $LOCAL
 echo "$dolarCOMERCIALdata" >> $LOCAL
 echo '$'"$qtddDOLARCOMERCIAL"' custam R$'"$total" >> $LOCAL
@@ -49,7 +48,7 @@ echo "Caso não deseje, apenas deixe em branco e pressione Enter."
 echo "Seu caminho atual de padrao esta em $PWD"
 read -p "Local onde quer salvar/nome do arquivo: " LOCAL
 test "$LOCAL" = "" && exit
-echo > $LOCAL
+echo >> $LOCAL
 echo "==={ DOLAR TURISMO }===" >> $LOCAL
 echo "$dolarTURISMOdata" >> $LOCAL
 echo '$'"$qtddDOLARTURISMO"' custam R$'"$total" >> $LOCAL
